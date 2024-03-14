10eeea7f41fe788ae31aa77c8c3b22a41f223afc
raihoney15 <127507565+raihoney15@users.noreply.github.com>
2024-03-14 17:27:51 +0530

    config/master.key

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,7 +49,7 @@ set :rails_env, 'production'
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, 'config/master.key'
+append :linked_files, 'config/master.key'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
