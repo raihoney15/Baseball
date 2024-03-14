883ef35b487682e46836e7ec78ee152021f32c25
raihoney15 <127507565+raihoney15@users.noreply.github.com>
2024-03-14 15:18:35 +0530

    latest2

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,7 +49,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/master.key'
+# append :linked_files, 'config/master.key'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
